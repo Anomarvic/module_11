86fad855dee316975af22c083b2c0e2062cdf701
Anomarvic <anomarvic@gmail.com>
2025-01-09 13:50:45 +0300

    +add homework11_2 внесла исправление

--- module_11_2.py
+++ module_11_2.py
@@ -5,7 +5,7 @@ def introspection_info(obj):
     obj_type = type(obj)
     attributes = dir(obj)
     methods = [method for method in dir(obj) if callable(getattr(obj, method))]
-    module = inspect.getmodule(obj)
+    module = inspect.getmodule(introspection_info)
     # словарь с информацией об объекте
     info = {
         'Тип объекта': obj_type,
